80dfb5d72748a17430d48b95a29c799b158743d0
Dave Swindell <cdswindell@comcast.net>
2025-01-27 16:55:14 -0500

    API Checkpoint

--- src/pytrain_api/fastapi_ex.py
+++ src/pytrain_api/fastapi_ex.py
@@ -5,23 +5,151 @@
 #
 #  SPDX-License-Identifier: LPGL
 #
-from typing import TypeVar
+from enum import Enum
+from typing import TypeVar, Annotated, Any
 
-from fastapi import FastAPI, HTTPException, APIRouter
+from fastapi import FastAPI, HTTPException, APIRouter, Path, Query
 from fastapi_utils.cbv import cbv
-from pytrain import CommandScope, TMCC1SwitchCommandEnum, CommandReq, TMCC1HaltCommandEnum
+from pydantic import BaseModel, field_validator, model_validator
+from pytrain import CommandScope, TMCC1SwitchCommandEnum, CommandReq, TMCC1HaltCommandEnum, PROGRAM_NAME
 from pytrain.cli.pytrain import PyTrain
 from pytrain.db.component_state import ComponentState
 from pytrain.protocol.command_def import CommandDefEnum
+from starlette.responses import RedirectResponse
 
 E = TypeVar("E", bound=CommandDefEnum)
+API_NAME = "PyTrainApi"
 
 pytrain = PyTrain("-client -api -echo".split())
 app = FastAPI()
 router = APIRouter()
 
 
-@app.get("/system/halt")
+@app.get("/", summary=f"Redirect to {API_NAME} Documentation")
+def redirect_to_new_url():
+    return RedirectResponse(url="/docs")
+
+
+class Component(str, Enum):
+    ACCESSORY = "accessory"
+    ENGINE = "engine"
+    ROUTE = "route"
+    SWITCH = "switch"
+    TRAIN = "train"
+
+
+class ComponentInfo(BaseModel):
+    tmcc_id: int
+    road_name: str | None
+    road_number: str | None
+    scope: Component
+
+
+C = TypeVar("C", bound=ComponentInfo)
+
+
+class RouteSwitch(BaseModel):
+    switch: int
+    position: str
+
+
+class RouteInfo(ComponentInfo):
+    switches: dict[int, str] | None
+
+
+class SwitchInfo(ComponentInfo):
+    scope: Component = Component.SWITCH
+    state: str | None
+
+
+class AccessoryInfo(ComponentInfo):
+    # noinspection PyMethodParameters
+    @model_validator(mode="before")
+    def validate_model(cls, data: Any) -> Any:
+        if isinstance(data, dict):
+            for field in {"aux", "aux1", "aux2"}:
+                if field not in data:
+                    data[field] = None
+            if "block" in data:
+                data["aux"] = data["block"]
+                del data["block"]
+            if "type" not in data:
+                data["type"] = "accessory"
+        return data
+
+    # noinspection PyMethodParameters
+    @field_validator("scope", mode="before")
+    def validate_component(cls, v: str) -> str:
+        return "accessory" if v in {"acc", "sensor_track", "sensor track", "power_district", "power district"} else v
+
+    scope: Component = Component.ACCESSORY
+    type: str | None
+    aux: str | None
+    aux1: str | None
+    aux2: str | None
+
+
+class EngineInfo(ComponentInfo):
+    scope: Component = Component.ENGINE
+    control: str | None
+    direction: str | None
+    engine_class: str | None
+    engine_type: str | None
+    labor: int | None
+    max_speed: int | None
+    momentum: int | None
+    rpm: int | None
+    smoke: str | None
+    sound_type: str | None
+    speed: int | None
+    speed_limit: int | None
+    train_brake: int | None
+    year: int | None
+
+
+class TrainInfo(EngineInfo):
+    scope: Component = Component.TRAIN
+
+
+@app.post("/{component}/{tmcc_id:int}/cli_req")
+async def send_command(
+    component: Component,
+    tmcc_id: Annotated[
+        int,
+        Path(
+            title="TMCC ID",
+            description="TMCC ID of the component to control",
+            ge=1,
+            le=99,
+        ),
+    ],
+    command: Annotated[str, Query(description=f"{PROGRAM_NAME} CLI command")],
+    is_tmcc: Annotated[str | None, Query(description="Send TMCC-style commands")] = None,
+):
+    try:
+        if component in [Component.ENGINE, Component.TRAIN]:
+            tmcc = " -tmcc" if is_tmcc is not None else ""
+        else:
+            tmcc = ""
+        cmd = f"{component.value} {tmcc_id}{tmcc} {command}"
+        parse_errors = pytrain.parse_cli(cmd)
+        if parse_errors:
+            raise HTTPException(status_code=422, detail=f"Command is invalid: {parse_errors}")
+        else:
+            # otherwise, send command
+            pytrain.queue_command(cmd)
+            return {"status": f"'{component.value} {tmcc_id} {command}' command sent"}
+    except HTTPException as he:
+        raise he
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@app.get(
+    "/system/halt",
+    summary="Emergency Stop",
+    description="Stops all engines and trains, in their tracks; turns off all power districts",
+)
 async def halt():
     try:
         CommandReq(TMCC1HaltCommandEnum.HALT).send()
@@ -30,7 +158,7 @@ async def halt():
         raise HTTPException(status_code=400, detail=str(e))
 
 
-@app.post("/system/stop-req")
+@app.post("/system/stop_req")
 async def stop():
     pytrain.queue_command("tr 99 -s")
     pytrain.queue_command("en 99 -s")
@@ -38,7 +166,7 @@ async def stop():
     return {"status": "Stop all engines and trains command sent"}
 
 
-@app.post("/system/echo-req")
+@app.post("/system/echo_req")
 async def echo(on: bool = True):
     pytrain.queue_command(f"echo {'on' if on else 'off'}")
     return {"status": f"Echo {'enabled' if on else 'disabled'}"}
@@ -46,50 +174,69 @@ async def echo(on: bool = True):
 
 def get_components(
     scope: CommandScope,
-    name: str = None,
+    contains: str = None,
     is_legacy: bool = None,
     is_tmcc: bool = None,
 ) -> list[dict[str, any]]:
     states = pytrain.store.query(scope)
     if states is None:
-        HTTPException(status_code=404, detail=f"No {scope} found")
+        raise HTTPException(status_code=404, detail=f"No {scope.label} found")
     else:
         ret = list()
         for state in states:
             if is_legacy is not None and state.is_legacy != is_legacy:
                 continue
+            print(f"State: {state} is TMCC: {state.is_tmcc} is Legacy: {state.is_legacy} ({is_tmcc})")
             if is_tmcc is not None and state.is_tmcc != is_tmcc:
                 continue
             # noinspection PyUnresolvedReferences
-            if name and state.name and name.lower() not in state.name.lower():
+            if contains and state.name and contains.lower() not in state.name.lower():
                 continue
             ret.append(state.as_dict())
+        if not ret:
+            raise HTTPException(status_code=404, detail=f"No matching {scope.label} found")
         return ret
 
 
-@app.get("/engines")
-async def get_engines(name: str = None, is_legacy: bool = None, is_tmcc: bool = None):
-    return get_components(CommandScope.ENGINE, name=name, is_legacy=is_legacy, is_tmcc=is_tmcc)
+@app.get("/engines", response_model=list[EngineInfo])
+async def get_engines(contains: str = None, is_legacy: bool = None, is_tmcc: bool = None):
+    return [
+        EngineInfo(**d)
+        for d in get_components(
+            CommandScope.ENGINE,
+            is_legacy=is_legacy,
+            is_tmcc=is_tmcc,
+            contains=contains,
+        )
+    ]
 
 
-@app.get("/trains")
-async def get_trains(name: str = None, is_legacy: bool = None, is_tmcc: bool = None):
-    return get_components(CommandScope.TRAIN, name=name, is_legacy=is_legacy, is_tmcc=is_tmcc)
+@app.get("/trains", response_model=list[TrainInfo])
+async def get_trains(contains: str = None, is_legacy: bool = None, is_tmcc: bool = None):
+    return [
+        TrainInfo(**d)
+        for d in get_components(
+            CommandScope.TRAIN,
+            is_legacy=is_legacy,
+            is_tmcc=is_tmcc,
+            contains=contains,
+        )
+    ]
 
 
-@app.get("/switches")
-async def get_switches(name: str = None):
-    return get_components(CommandScope.SWITCH, name=name)
+@app.get("/switches", response_model=list[SwitchInfo])
+async def get_switches(contains: str = None):
+    return [SwitchInfo(**d) for d in get_components(CommandScope.SWITCH, contains=contains)]
 
 
-@app.get("/accessories")
-async def get_accessories(name: str = None):
-    return get_components(CommandScope.ACC, name=name)
+@app.get("/accessories", response_model=list[AccessoryInfo])
+async def get_accessories(contains: str = None):
+    return [AccessoryInfo(**d) for d in get_components(CommandScope.ACC, contains=contains)]
 
 
-@app.get("/routes")
-async def get_routes(name: str = None):
-    return get_components(CommandScope.ROUTE, name=name)
+@app.get("/routes", response_model=list[RouteInfo])
+async def get_routes(contains: str = None):
+    return [RouteInfo(**d) for d in get_components(CommandScope.ROUTE, contains=contains)]
 
 
 class PyTrainComponent:
@@ -101,7 +248,7 @@ class PyTrainComponent:
     def scope(self) -> CommandScope:
         return self._scope
 
-    def get(self, tmcc_id: int):
+    def get(self, tmcc_id: int) -> dict[str, Any]:
         state: ComponentState = pytrain.store.query(self.scope, tmcc_id)
         if state is None:
             raise HTTPException(status_code=404, detail=f"{self.scope.title} {tmcc_id} not found")
@@ -168,23 +315,29 @@ class Engine(PyTrainEngine):
     def __init__(self):
         super().__init__(CommandScope.ENGINE)
 
-    @router.get("/engine/{tmcc_id:int}")
-    async def get_engine(self, tmcc_id: int):
-        return super().get(tmcc_id)
+    @router.get("/engine/{tmcc_id:int}", response_model=EngineInfo)
+    async def get_engine(self, tmcc_id: int = Path(description="Engine's TMCC ID", ge=1, le=99)):
+        return EngineInfo(**super().get(tmcc_id))
 
-    @router.post("/engine/{tmcc_id:int}/speed-req/{speed:int}")
-    async def set_speed(self, tmcc_id: int, speed: int, immediate: bool = False, dialog: bool = False):
+    @router.post("/engine/{tmcc_id:int}/speed_req/{speed:int}")
+    async def set_speed(
+        self,
+        tmcc_id: Annotated[int, Path(description="Engine's TMCC ID", ge=1, le=99)],
+        speed: Annotated[int, Path(description="New speed", ge=0, le=199)],
+        immediate: bool = False,
+        dialog: bool = False,
+    ):
         return super().speed(tmcc_id, speed, immediate=immediate, dialog=dialog)
 
-    @router.post("/engine/{tmcc_id:int}/startup-req")
+    @router.post("/engine/{tmcc_id:int}/startup_req")
     async def startup(self, tmcc_id: int, dialog: bool = False):
         return super().startup(tmcc_id, dialog=dialog)
 
-    @router.post("/engine/{tmcc_id:int}/shutdown-req")
+    @router.post("/engine/{tmcc_id:int}/shutdown_req")
     async def shutdown(self, tmcc_id: int, dialog: bool = False):
         return super().shutdown(tmcc_id, dialog=dialog)
 
-    @router.post("/engine/{tmcc_id:int}/stop-req")
+    @router.post("/engine/{tmcc_id:int}/stop_req")
     async def stop(self, tmcc_id: int):
         return super().stop(tmcc_id)
 
@@ -194,23 +347,23 @@ class Train(PyTrainEngine):
     def __init__(self):
         super().__init__(CommandScope.TRAIN)
 
-    @router.get("/train/{tmcc_id:int}")
+    @router.get("/train/{tmcc_id:int}", response_model=TrainInfo)
     async def get_train(self, tmcc_id: int):
-        return super().get(tmcc_id)
+        return TrainInfo(**super().get(tmcc_id))
 
-    @router.post("/train/{tmcc_id:int}/speed-req/{speed:int}")
+    @router.post("/train/{tmcc_id:int}/speed_req/{speed:int}")
     async def set_speed(self, tmcc_id: int, speed: int, immediate: bool = False, dialog: bool = False):
         return super().speed(tmcc_id, speed, immediate=immediate, dialog=dialog)
 
-    @router.post("/train/{tmcc_id:int}/startup-req")
+    @router.post("/train/{tmcc_id:int}/startup_req")
     async def startup(self, tmcc_id: int, dialog: bool = False):
         return super().startup(tmcc_id, dialog=dialog)
 
-    @router.post("/train/{tmcc_id:int}/shutdown-req")
+    @router.post("/train/{tmcc_id:int}/shutdown_req")
     async def shutdown(self, tmcc_id: int, dialog: bool = False):
         return super().shutdown(tmcc_id, dialog=dialog)
 
-    @router.post("/train/{tmcc_id:int}/stop-req")
+    @router.post("/train/{tmcc_id:int}/stop_req")
     async def stop(self, tmcc_id: int):
         return super().stop(tmcc_id)
 
@@ -220,15 +373,15 @@ class Switch(PyTrainComponent):
     def __init__(self):
         super().__init__(CommandScope.SWITCH)
 
-    @router.get("/switch/{tmcc_id}")
-    async def get_switch(self, tmcc_id: int):
-        return super().get(tmcc_id)
+    @router.get("/switch/{tmcc_id}", response_model=SwitchInfo)
+    async def get_switch(self, tmcc_id: int) -> SwitchInfo:
+        return SwitchInfo(**super().get(tmcc_id))
 
-    @router.post("/switch/{tmcc_id}/thru-req")
+    @router.post("/switch/{tmcc_id}/thru_req")
     async def thru(self, tmcc_id: int):
         return self.send(TMCC1SwitchCommandEnum.THRU, tmcc_id)
 
-    @router.post("/switch/{tmcc_id}/out-req")
+    @router.post("/switch/{tmcc_id}/out_req")
     async def out(self, tmcc_id: int):
         return self.send(TMCC1SwitchCommandEnum.OUT, tmcc_id)
 
@@ -238,9 +391,9 @@ class Accessory(PyTrainComponent):
     def __init__(self):
         super().__init__(CommandScope.ACC)
 
-    @router.get("/accessory/{tmcc_id}")
+    @router.get("/accessory/{tmcc_id}", response_model=AccessoryInfo)
     async def get_accessory(self, tmcc_id: int):
-        return super().get(tmcc_id)
+        return AccessoryInfo(**super().get(tmcc_id))
 
 
 @cbv(router)
@@ -248,11 +401,11 @@ class Route(PyTrainComponent):
     def __init__(self):
         super().__init__(CommandScope.ROUTE)
 
-    @router.get("/route/{tmcc_id}")
+    @router.get("/route/{tmcc_id}", response_model=RouteInfo)
     async def get_route(self, tmcc_id: int):
-        return super().get(tmcc_id)
+        return RouteInfo(**super().get(tmcc_id))
 
-    @router.get("/route/{tmcc_id}/fire-req")
+    @router.get("/route/{tmcc_id}/fire_req")
     async def fire(self, tmcc_id: int):
         return super().get(tmcc_id)
 
